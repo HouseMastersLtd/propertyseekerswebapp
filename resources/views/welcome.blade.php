90d02e7578a0a478774304b95eab4440b58e49d3
thecomputermanonline <olalekanarowoselu@gmail.com>
2021-07-25 16:12:53 +0400

    add users to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,16 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="border-t-8 border-blue-500 p-32">
+    <div class="max-w-4xl mx-auto prose xl:prose-xl">
+    <h1>welcome to all properties in mauritius</h1>
+    <p class ="mb-20">lots of property by</p>
+    @foreach($users as $user)
+    <h2>{{$user->name}}</h2>
+    @endforeach
+    </div>
+    </div>
+        <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -127,6 +136,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     $users = \App\Models\User::all();
-    dd($users);
-    return view('welcome');
+    
+    return view('welcome', ['users' => $users]);
 });
